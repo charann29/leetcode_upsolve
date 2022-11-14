9c121522127ba9698d6466de76aa69ef375c45fc
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2022-11-14 23:20:51 +0530

    Add files via upload

---
+++ Two_Sum II LC.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int i = 0, j = numbers.size() - 1;
+        while (i < j)
+        {
+            if (numbers[i] + numbers[j] == target)
+            {
+                break;
+            }
+            else if (numbers[i] + numbers[j] < target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return {i + 1, j + 1};
+    }
+};
