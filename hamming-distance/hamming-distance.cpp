2d1b09c712d980c7534969038e1362bf11034976
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2023-06-12 18:37:52 +0530

    Time: 3 ms (36.71%) | Memory: 5.9 MB (92.46%) - LeetSync

---
+++ hamming-distance/hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result =x^y;
+        int count=0;
+        while(result){
+            result = result &(result-1);
+            count++;
+        }
+        return count;
+    }
+};
