aa88c36d65db97a76474d618fff1f87bc9a4e017
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2023-06-12 17:44:51 +0530

    Time: 4 ms (60.10%) | Memory: 6.7 MB (23.85%) - LeetSync

---
+++ find-the-difference/find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+            unordered_map<char, int> count;
+            for (char c : s) {
+                count[c]++;
+            }
+            for (char c : t) {
+                if (count.find(c) == count.end() || count[c] == 0) {
+                    return c;
+                }
+                count[c]--;
+            }
+            return '\0';
+    }
+};
