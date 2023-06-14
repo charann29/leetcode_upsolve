289e9f3c077667e3aa6359d315e2c21bede9e111
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2023-06-15 04:49:31 +0530

    Time: 16 ms (5.15%) | Memory: 8.4 MB (44.29%) - LeetSync

--- make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,20 +1,34 @@
-class Solution 
-{
-    public:
-        int minimumOperations(vector<int>& nums) 
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums)
+    {
+        priority_queue <int, vector<int>, greater<int>> pq;
+        vector <int> temp;
+        int k,ans=0;
+        for(auto i:nums)
+            pq.push(i);
+        while(!pq.empty())
         {
-            int cnt = 0, N = nums.size();
-            sort(nums.begin(), nums.end());
-
-            for(int i=0; i<N; i++)
+            if(pq.top()==0)
             {
-                if(nums[i] > 0)
-                {
-                    int num = nums[i];
-                    cnt++;
-                    for(int j=i; j<N; j++) nums[j] -= num;
-                }
+                pq.pop();
+                continue;
             }
-            return cnt;
+            k=pq.top();
+            pq.pop();
+            while(!pq.empty())
+            {
+                int x=pq.top();
+                temp.push_back(x-k);
+                pq.pop();
+            }
+            ans++;
+            for(auto i:temp)
+            {
+                pq.push(i);
+            }
+            temp.clear();
         }
-}; 
+        return ans;
+    }
+};
