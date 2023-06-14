67fa1c6ab1af3f35100c6b84b6c700530a27d7be
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2023-06-15 01:41:16 +0530

    Time: 48 ms (5.84%) | Memory: 15.9 MB (6.74%) - LeetSync

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
@@ -8,7 +7,7 @@ public:
 
         for (int i = 0; i < n; ++i) {
             if (!visited[i]) {
-                bfs(isConnected, i, visited);
+                dfs(isConnected, i, visited);
                 provinces++;
             }
         }
@@ -17,19 +16,19 @@ public:
     }
 
 private:
-    void bfs(const vector<vector<int>>& isConnected, int city, vector<bool>& visited) {
+    void dfs(const vector<vector<int>>& isConnected, int city, vector<bool>& visited) {
         int n = isConnected.size();
-        queue<int> queue;
-        queue.push(city);
+        stack<int> stack;
+        stack.push(city);
 
-        while (!queue.empty()) {
-            int currCity = queue.front();
-            queue.pop();
+        while (!stack.empty()) {
+            int currCity = stack.top();
+            stack.pop();
             visited[currCity] = true;
 
             for (int neighbor = 0; neighbor < n; ++neighbor) {
                 if (isConnected[currCity][neighbor] == 1 && !visited[neighbor]) {
-                    queue.push(neighbor);
+                    stack.push(neighbor);
                 }
             }
         }
