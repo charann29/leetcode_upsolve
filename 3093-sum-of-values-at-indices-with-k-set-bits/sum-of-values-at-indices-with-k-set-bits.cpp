3543483e831d003d071bc0644276ee82411558cd
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2024-07-14 00:52:02 +0530

    Time: 7 ms (77.41%) | Memory: 25 MB (5.84%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -4,15 +4,9 @@ public:
         int n = nums.size();
         int sum = 0;
         
-        // Precompute the number of set bits for all possible indices
-        vector<int> bitCounts(1 << 20, 0); // Assuming nums.size() <= 2^20 for practicality
-        for (int i = 1; i < (1 << 20); ++i) {
-            bitCounts[i] = bitCounts[i >> 1] + (i & 1);
-        }
-        
         // Iterate through all indices and sum the values where the index has k set bits
         for (int i = 0; i < n; ++i) {
-            if (bitCounts[i] == k) {
+            if (__builtin_popcount(i) == k) {
                 sum += nums[i];
             }
         }
