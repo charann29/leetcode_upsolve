a2d86e869e81435e4d5a425c0cd38f1fe9c86220
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2025-01-22 19:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (46.25%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -6,26 +6,30 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.totalSum = 0
-    totalSum =0
-    def dfs(self ,root,currentSum ):
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            currentSum = (currentSum*10) + (root.val)
-            self.totalSum +=currentSum
-            # print(self.totalSum)
-        else:
-            currentSum = (currentSum*10) + (root.val)
-            self.dfs(root.left,currentSum)
-            self.dfs(root.right,currentSum)
-        return self.totalSum
+        self.totalSum = 0 # global varaible
 
+    def helper(self,root,ans):
+        if not root :  #no node
+            return 0
+        if not root.left and not root.right: # leaf node 
+            ans = (ans*10)+root.val
+            self.totalSum = self.totalSum+ans
+        else :       # node in between
+           ans = (ans*10)+root.val
+           self.helper(root.left,ans)
+           self.helper(root.right,ans)
+        return self.totalSum 
 
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        return self.helper(root,ans)
+
+
+
+
        
-        currentSum =0
-        return self.dfs(root,currentSum)
+        # currentSum =0
+        # return self.dfs(root,currentSum)
 
 
 
